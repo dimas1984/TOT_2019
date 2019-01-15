02755b5034a3ba869a859c9a68591b97509e088b
arie.rachmad.s@gmail.com <arie.rachmad.s@gmail.com>
2019-01-16 01:09:20 +0700

    add code1

---
+++ code/01/oshop1/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDBPUYjTMYCAYNHVItpjTZmqAmnIjAvPag",
+    authDomain: "oshop-69b6a.firebaseapp.com",
+    databaseURL: "https://oshop-69b6a.firebaseio.com",
+    projectId: "oshop-69b6a",
+    storageBucket: "oshop-69b6a.appspot.com",
+    messagingSenderId: "1009638802566"
+  }
+};
