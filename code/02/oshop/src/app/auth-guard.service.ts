63daac63de566965fede444d84221a8c759dedb6
dimas1984 <dimas.w@polinema.ac.id>
2019-01-20 12:13:47 +0700

    revisi code

--- code/02/oshop/src/app/auth-guard.service.ts
+++ code/02/oshop/src/app/auth-guard.service.ts
@@ -14,14 +14,14 @@ export class AuthGuard implements CanActivate{
   
   constructor(private auth:AuthService,private router:Router) { }
 
-    canActivate( route:ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean
+    canActivate(): Observable<boolean> | Promise<boolean> | boolean
     {
       return this.auth.user$.pipe(
         map(user =>{
           if (user){
             return true;
           } 
-          this.router.navigate(["/login"]),{queryParams:{returnUrl:state.url}};
+          this.router.navigate(["/login"]);
           return false;
         })
       );
