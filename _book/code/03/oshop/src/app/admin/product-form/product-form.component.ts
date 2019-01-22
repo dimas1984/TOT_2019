190fd0d10902f24fe3cfa1e2dfb47df713c14f5f
dimas1984 <dimas.w@polinema.ac.id>
2019-01-22 22:07:25 +0700

    edit-product

--- _book/code/03/oshop/src/app/admin/product-form/product-form.component.ts
+++ _book/code/03/oshop/src/app/admin/product-form/product-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from 'src/app/category.service';
 import { ProductService } from 'src/app/product.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -11,12 +11,22 @@ import { Router } from '@angular/router';
 export class ProductFormComponent implements OnInit {
 
   categories$;
+  product={};
 
   constructor(
     private router:Router,
+    private route: ActivatedRoute,
     private categoryService:CategoryService,
     private productService:ProductService) {
     this.categories$=categoryService.getCategories();
+
+    // let id = this.route.snapshot.paramMap.get('id');
+    // if (id) this.router.navigate(['/admin/admin-products']);
+    //  if (id) this.productService.get(id).take(1).subscribe(p=>this.product=p)
+    let id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.productService.get(id).valueChanges().subscribe(p => this.product = p);
+    }
    }
 
    save(product){
